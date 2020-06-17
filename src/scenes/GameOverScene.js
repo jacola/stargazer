226b50f5ed707ef2cb8664874c7665c918813327
Jacques Lalancette <me@jacques.io>
2020-06-17 21:50:58 +0900

    Game over screen

--- src/scenes/GameOverScene.js
+++ src/scenes/GameOverScene.js
@@ -5,12 +5,40 @@ export class GameOverScene extends Phaser.Scene {
     super({ key: SCENES.GAME_OVER });
   }
 
-  preload() {
+  create() {
     const { width, height } = this.cameras.main;
-    this.add.rectangle(width / 2, height / 2, width, height, 0xff0000);
-  }
+    const bg = this.add.rectangle(
+      width / 2,
+      height / 2,
+      width,
+      height,
+      0x111111
+    );
 
-  creater() {}
+    const playAgain = this.add
+      .sprite(width / 2, height / 2, "entities", "UI/playagain")
+      .setScale(4)
+      .setVisible(false);
+
+    setTimeout(() => {
+      bg.setInteractive();
+      bg.on("pointerup", () => this.scene.start(SCENES.GAME));
+      playAgain.setVisible(true);
+    }, 1000);
+
+    this.add
+      .sprite(width / 2, height / 4, "entities", "UI/score")
+      .setScale(6)
+      .setOrigin(0.5, 1);
+
+    const score = this.add.bitmapText(
+      width / 2,
+      height / 4,
+      "Starfont",
+      this.registry.score
+    );
+    score.setOrigin(0.5, 0).setScale(2).setTint(0x00ff00);
+  }
 
   update() {}
 }
